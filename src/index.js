22aba8b4f993cf15d1c0dbe207a3d90305c9a0ae
KoTuKJuBoTuK <simonovantond@gmail.com>
2018-09-11 01:45:40 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =  function check(str, bracketsConfig) {
+  let arr = [];
+  for(let i = 0; i < str.length; i++){
+    if(str[i] == '(' || str[i] == '[' || str[i] == '{'){
+      arr.push(str[i]);
+    }
+    else if(str[i] == ')' || str[i] == ']' || str[i] == '}'){
+      arr.pop();
+    }
+  }
+  if(arr.length == 0) {
+    return true
+  }
+  else{
+    return false;
+  }
 }
